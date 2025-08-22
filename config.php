e71b521aca1a88739f68335adff031e45e5ed56d
Neek Dev <nbee19347@gmail.com>
2025-08-23 00:32:09 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'your_db_user');
+define('DB_PASS', 'your_db_pass');
+define('DB_NAME', 'crypto_mining');
+
+define('COINPAYMENTS_API_KEY', 'your_api_key');
+define('COINPAYMENTS_IPN_SECRET', 'your_ipn_secret');
+define('MIN_WITHDRAWAL', 50);
+define('REFERRAL_COMMISSION', 0.15);
+?>
